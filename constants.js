4a6080e35aa49cd464326aea42b632cf4dce2ead
Jolboldu <sydykalyuulu_z@auca.kg>
2019-09-22 15:31:48 +0600

    changed redirect url for twitch

--- constants.js
+++ constants.js
@@ -2,5 +2,7 @@ module.exports = {
 	'TWITCH_CLIENT_ID' : '5j36rp610vnq33b425ayzd7d8juv23',
 	'TWITCH_SECRET'    : 'rfmqvzuunfs4jivuofi8eomvyddhpl',
 	'SESSION_SECRET'   : 'sosilalka',
-	'CALLBACK_URL'     : 'http://localhost:3000/auth/twitch/redirect',
+//	'CALLBACK_URL'     : 'http://localhost:3000/auth/twitch/redirect',
+	'CALLBACK_URL'     : 'http://165.22.71.126/auth/twitch/redirect',
+
 }
