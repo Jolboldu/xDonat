f209017355cc04fb23b236b9ac2bd0cbd483c332
Beksultan <beksultan.dev@gmail.com>
2019-09-28 14:45:52 +0600

    Donates

--- routes/wallets.js
+++ routes/wallets.js
@@ -24,13 +24,34 @@ router.get('/yandex', authCheck, (req, res) => {
 });
 
 // these kinda 'get' has to be in the Streamer's page, should be deleted next commit
-// router.get('/yandex/pay', (req, res) => {
-//     res.render('form');
-// });
+router.get('/yandex/pay', (req, res) => {
+    res.render('form');
+});
 
 // handling HTTP requests from Yandex after success payment to streamer
 router.post('/yandex/requests', (req, res) => {
     console.log(req.body)
+
+    // парсим поле label и получаем адрес стримера
+
+    // если пришли деньги
+    paymentAccepted = true;
+    if (paymentAccepted) {
+        models.User.findOne({ username: req.params.username }, (err, user_data) => {
+            if (err) throw err;
+            console.log(user_data);
+            
+            // if (user_data) {
+            //     res.render('streamer', { user_data: user_data});
+            // }
+            // else
+            // {
+            //     // Have to raise ERROR 404, for now just rendering with messages
+            //     res.render('error');
+            // }
+        });
+
+    }
 });
 
 module.exports = router;
