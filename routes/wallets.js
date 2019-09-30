3d7603099f0df26646906445b3482c50c7ac64e1
Jolboldu <sydykalyuulu_z@auca.kg>
2019-09-30 12:12:34 +0600

    without function

--- routes/wallets.js
+++ routes/wallets.js
@@ -84,6 +84,7 @@ router.post('/yandex/requests', (req, res) => {
         models.YandexWallet.find({addressOfWallet : data.reciever}, (error, wallet)=>{
             var wholeInfo = req.body.notification_type + "&" + req.body.operation_id + "&" + req.body.amount + "&" + req.body.currency + "&" + req.body.datetime + "&" + req.body.sender + "&" + req.body.codepro + "&" + wallet.secretOfWallet + "&" + req.body.label;
             if(req.body.sha1_hash == sha1(wholeInfo)){
+                console.log("hashes are equal")
                 paymentAccepted = true;    
                 handleRequest(paymentAccepted, req, res)       
             }
