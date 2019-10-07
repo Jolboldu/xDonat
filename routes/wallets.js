6584de0df3bc24eef2a97fd55643875e411fe054
Jolboldu <sydykalyuulu_z@auca.kg>
2019-10-07 18:00:06 +0600

    should work hashes

--- routes/wallets.js
+++ routes/wallets.js
@@ -76,24 +76,19 @@ router.post('/yandex/pay', (req, res) => {
 // handling HTTP requests from Yandex after success payment to streamer
 router.post('/yandex/requests', (req, res) => {    
     console.log(req.body);
-    models.Donate.find({_id: req.body.label}, (err, data) => {
-        console.log(data);
-        console.log(data.reciever);
-        models.YandexWallet.find({}, (error, wallet) => {
-            console.log(wallet);
-        })
-        // models.YandexWallet.find({addressOfWallet: data.reciever}, (error, wallet)=>{
-        //     var wholeInfo = req.body.notification_type + "&" + req.body.operation_id + "&" + req.body.amount + "&" + req.body.currency + "&" + req.body.datetime + "&" + req.body.sender + "&" + req.body.codepro + "&" + wallet.secretOfWallet + "&" + req.body.label;
-        //     if(req.body.sha1_hash == sha1(wholeInfo)){
-        //         console.log("hashes are equal")
-        //         paymentAccepted = true;    
-        //         handleRequest(paymentAccepted, req, res)       
-        //     }else{
-        //         console.log("wrong hashes");
-        //         console.log(wallet.secretOfWallet);
-        //     }
+    models.Donate.findOne({_id: req.body.label}, (err, data) => {
+        models.YandexWallet.findOne({addressOfWallet: data.reciever}, (error, wallet)=>{
+            var wholeInfo = req.body.notification_type + "&" + req.body.operation_id + "&" + req.body.amount + "&" + req.body.currency + "&" + req.body.datetime + "&" + req.body.sender + "&" + req.body.codepro + "&" + wallet.secretOfWallet + "&" + req.body.label;
+            if(req.body.sha1_hash == sha1(wholeInfo)){
+                console.log("hashes are equal")
+                paymentAccepted = true;    
+                handleRequest(paymentAccepted, req, res)       
+            }else{
+                console.log("wrong hashes");
+                console.log(wallet.secretOfWallet);
+            }
             
-        // })
+        })
     })    
 });
 
