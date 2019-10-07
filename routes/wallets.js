fac133f24d2712a57500cf555ac4b769a149af46
Jolboldu <sydykalyuulu_z@auca.kg>
2019-10-07 16:42:22 +0600

    check number and string comparison(2)

--- routes/wallets.js
+++ routes/wallets.js
@@ -80,8 +80,8 @@ router.post('/yandex/requests', (req, res) => {
         console.log("donates data");
         console.log(data);
         models.YandexWallet.find({addressOfWallet : data.reciever }, (error, wallet)=>{
-            if(err)
-                console.log(err)
+            if(error)
+                console.log(error)
             else{
                 var wholeInfo = req.body.notification_type + "&" + req.body.operation_id + "&" + req.body.amount + "&" + req.body.currency + "&" + req.body.datetime + "&" + req.body.sender + "&" + req.body.codepro + "&" + wallet.secretOfWallet + "&" + req.body.label;
                 if(req.body.sha1_hash == sha1(wholeInfo)){
