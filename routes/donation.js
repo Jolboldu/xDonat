78887ab8b17890e54141c168573c7c852febf134
Beksultan <beksultan.dev@gmail.com>
2019-10-08 18:19:20 +0600

    returned old version without method textDonate()

--- routes/donation.js
+++ routes/donation.js
@@ -20,12 +20,7 @@ module.exports = function (io) {
 
   router.get('/text_donate', (req, res) => {
     res.render('donation/text');
-    df = 'fdjhs';
-    textDonate(df);
-  });
-
-  module.exports.textDonate = (data) => {
-    var isDonation = true
+        var isDonation = true
 
     if(isDonation)
     {
@@ -44,7 +39,7 @@ module.exports = function (io) {
        const client = new textToSpeech.TextToSpeechClient();
        // The text to synthesize
        // const text = req.body.text
-       const text = 'Привет'
+       const text = 'Привет мир'
        //Construct the request
        const request = {
          input: {text: text},
@@ -72,9 +67,59 @@ module.exports = function (io) {
 
     }
 
-module.exports = textDonate();
+  });
 
-  };
+//   module.exports.textDonate = (data) => {
+//     var isDonation = true
+
+//     if(isDonation)
+//     {
+//     // таймАут стоит временно, чтобы проверить работу Сокетов
+//     setTimeout(function() {     
+
+//       // userid это рум
+//     room = "abc123";
+//     // хочу отправить сообщение в данный рум
+//     var data = '{"donater": "akunMata", "text": "привет Аниме, как твои дела, покажи сиськи", "amount": 149}'
+//     io.to(room).emit('message', data) 
+
+//     // Function to Work with sounds
+//       async function main() {
+//        // Creates a client
+//        const client = new textToSpeech.TextToSpeechClient();
+//        // The text to synthesize
+//        // const text = req.body.text
+//        const text = 'Привет'
+//        //Construct the request
+//        const request = {
+//          input: {text: text},
+//          // Select the language and SSML Voice Gender (optional)
+//          voice: {languageCode: 'ru-RU', ssmlGender: 'MALE'},
+//          // Select the type of audio encoding
+//          audioConfig: {audioEncoding: 'MP3'},
+//        };
+//        // // Performs the Text-to-Speech request
+//        const [response] = await client.synthesizeSpeech(request);
+//        // Write the binary audio content to a local file
+//        const writeFile = util.promisify(fs.writeFile);
+//        await writeFile('output.mp3', response.audioContent, 'binary');
+
+//        console.log('Audio content written to file: output.mp3');
+//        // res.sendStatus(200);
+//       }
+//       main();
+
+
+
+
+
+//     }, 5000);
+
+//     }
+
+// module.exports = textDonate();
+
+//   };
 
 
 
