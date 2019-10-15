930620331bd000b3e373c0579d0f834483235e60
Jolboldu <sydykalyuulu_z@auca.kg>
2019-10-16 04:02:55 +0600

    trying to get my donate stats

--- routes/stats.js
+++ routes/stats.js
@@ -13,13 +13,32 @@ const authCheck = (req, res, next) => {
 /* GET users listing. */
 
 router.get('/full', authCheck, function(req, res, next) {
-	var query = models.Donate.find({userId: req.user.id}, (err, data) => {
-		// models.PaymentYandex.find(donateId: data.)
+	var stats = [];
+
+	models.Donate.find({userId: req.user.id}, (err, data) => {
+		models.PaymentYandex.find({}, (error, doc) => {
+			for(i = 0; i < data.length; i++)
+			{
+				for(j = 0; j < doc.length; j++)
+				{	
+					if(data[i].id == doc[j].donateId)
+					{
+						stats.push(data[i]);
+						break;
+					}
+				}
+			}
+			res.send(stats);
+		})
 	});
+
+	// var query = models.Donate.find({userId: req.user.id}, (err, data) => {
+	// 	// models.PaymentYandex.find(donateId: data.)
+	// });
 	
-	query.then((data)=>{
-		res.send(data);
-	});
+	// query.then((data)=>{
+	// 	res.send(data);
+	// });
 });
 
 router.get('/', function(req, res, next) {
